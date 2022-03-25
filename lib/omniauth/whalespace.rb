f9025b026a8c7564ff9821ee7c041d80a2f05289
이창환 <lch@team-mono.com>
2022-03-25 16:54:46 +0900

    feat: initial commit

---
+++ lib/omniauth/whalespace.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "whalespace/version"
+
+module Omniauth
+  module Whalespace
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
